749d64d00a95a8255236d5eb16ae3cfec9c81eb8
Yuki Flores <45567338+XTremistGames@users.noreply.github.com>
2019-02-17 21:52:41 +0300

    Перенёс систему предупреждений в канал warning-sys

--- index.js
+++ index.js
@@ -3510,9 +3510,9 @@ bot.on('guildMemberUpdate', async (oldMember, newMember) => {
 		let member = await newMember.guild.members.get(entry.executor.id);
 		if(member.id == "159985870458322944" || member.id == "155149108183695360") return; // Игнор ботов-модераторов
 		let server = bot.guilds.find(g => g.id == 528635749206196232);
-		let author_bot = server.members.find(m => m.id == 408740341135704065);
-		if (!author_bot) return;
-		author_bot.send(`**Юки, привет! Держи отчет о подозрительном действии модератора!\nМодератор <@${member.id}> выдал роль ${role.name} пользователю <@${newMember.id}> **`);
+		let channel_warn = server.channels.find(c => c.name == "warning-system");
+		if (!channel_warn) return;
+		channel_warn.send(`<@&528637204055064587>\n**Привет, модераторы! Держите отчет о подозрительном действии модератора!\nМодератор <@${member.id}> выдал роль <@&${role.id}> пользователю <@${newMember.id}> **`);
 		return;
 	}
         if (role.name != "Spectator™" && role.name != "Support Team") return
@@ -3560,9 +3560,9 @@ bot.on('guildMemberUpdate', async (oldMember, newMember) => {
 		let member = await newMember.guild.members.get(entry.executor.id);
 		if(member.id == "159985870458322944" || member.id == "155149108183695360") return; // Игнор ботов-модераторов
 		let server = bot.guilds.find(g => g.id == 528635749206196232);
-		let author_bot = server.members.find(m => m.id == 408740341135704065);
-		if (!author_bot) return;
-		author_bot.send(`**Юки, привет! Держи отчет о подозрительном действии модератора!\nМодератор <@${member.id}> снял роль ${role.name} пользователю <@${newMember.id}> **`);
+		let channel_warn = server.channels.find(c => c.name == "warning-system");
+		if (!channel_warn) return;
+		channel_warn.send(`<@&528637204055064587>\n**Привет, модераторы! Держите отчет о подозрительном действии модератора!\nМодератор <@${member.id}> снял роль <@&${role.id}> пользователю <@${newMember.id}> **`);
 		return;
 	}
         if (role.name != "Spectator™" && role.name != "Support Team") return
