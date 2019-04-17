95752397831da38f5536c2ffbf087fa9bc8bd71a
Артём <artemka076.dimaneal@gmail.com>
2019-04-17 22:03:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const fs = require("fs");
 
-const version = '5.2.3';
+const version = '5.2.4';
 // Первая цифра означает глобальное обновление. (global_systems)
 // Вторая цифра обозначет обновление одной из подсистем. (команда к примеру)
 // Третяя цифра обозначает статус обновления [0 (develop), 1 (testing), 2 (fix), 3 (debug relese), 4 (relese)]
 
-const update_information = "Теперь будет писать при активном обновлении."
+const update_information = "Изменения отсутствуют."
 
 let levelhigh = 0;
 let lasttestid = 'net';
